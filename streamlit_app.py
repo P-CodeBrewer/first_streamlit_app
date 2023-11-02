1efcbe24519d6ecb62b7e6e45520e09eeaca52c6
Pratiksha Reddy <148791109+P-CodeBrewer@users.noreply.github.com>
2023-11-02 14:23:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 import pandas
 
 streamlit.title("Welcome to P-CodeBrewer's Smoothie Shop")
-streamlit.header("Build your own Smoothie!!!")
+streamlit.header("Build your own Smoothie!!!" )
