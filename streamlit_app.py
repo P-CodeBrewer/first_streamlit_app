0698da6e2a57a0f77b324d811c67e874aff6c8c5
Pratiksha Reddy <148791109+P-CodeBrewer@users.noreply.github.com>
2023-11-02 14:10:43 -0400

    dataframe_changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text('Kale, Spinach and Rocket Smoothie')
 
 streamlit.header("Build your own Smoothie!!")
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
+fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(fruit_list)
