c186a832f47b47875a0c3aa9da05bebab27ce61f
Pratiksha Reddy <148791109+P-CodeBrewer@users.noreply.github.com>
2023-11-02 14:21:44 -0400

    smottie_shop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit
-import pandas 
+import pandas
 
-streamlit.title("My Parents New Healthy Diner!!")
-streamlit.header("Breakfast Menu")
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.header("Build your own Smoothie!!")
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+streamlit.title("Welcome to P-CodeBrewer's Smoothie Shop")
+streamlit.header("Build your own Smoothie!!!")
